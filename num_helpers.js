25fd832286f74dda0a61b7d8185579c9f66fcd24
Arnold Ndiwalana <arnoldndiwalana@gmail.com>
2019-03-19 17:48:39 +0300

    counts the type of numbers and displays number for each type

--- num_helpers.js
+++ num_helpers.js
@@ -1,38 +1,59 @@
 // Function that loops through numbers from 0 to 100 and outputs the tyoe of number and uses other functions.
+// funnction outputs the count of the different type of numbers
 
 
 // Main function that calls all the helper functions
 const main = (m) => {
+
+  var countOdd = 0;  // variable to store count of odd numbers
+  var countEven = 0; // variable to store count of even numbers
+  var countPrime = 0; // variable to store count of prime numbers
+  var countFizz =0;  // variable to store count of multiples of  3
+  var countBuzz =0;  // variable to store count of multiple of 5
+  var countFizzBuzz =0; // variable to store count of multiples of 3 and 5 (multiples of 15)
   // loop to check the numbers and assign them the types
   for (num = 0; num<=m+1; num++) {
+
+
     // Array to store the types of numbers
     var finalArray = [];
 //check if the numbers are prime numbers and assign it the type
     if (primeNumber(num) == true) {
       finalArray.push("prime")
+      countPrime++;
     }
     //check if the numbers are multiples of 15 and assign it the type
     if(fizzbuzz(num) == true){
       finalArray.push("FizzBuzz")
+      countFizzBuzz++;
     }
     // check if numbers are multiples of 3 and assign it the type
     if(fizz(num) == true){
       finalArray.push("Fizz")
+      countFizz++;
     }
     // check if numbers are multiples of 5 and assign it the type
     if(buzz(num) == true){
       finalArray.push("Buzz")
+      countBuzz++;
     }
     // check if numbers are odd or even and assitgn it the type
     if (even_sort(num) == true) {
+
       finalArray.push("even")
+      countEven++;
     } else {
       finalArray.push("odd")
+      countOdd++;
+
 
     }
+
+// prints the numbers and their types
     console.log(num, finalArray);
   };
-
+  // prints the number count for the different types
+console.log("\n Number Count: " + "\n Odd Numbers: " +countOdd + "\n Even numbers :" +countEven + "\n Prime Numbers: " +countPrime + "\n Multiples of 3 : " +  countFizz + "\n Multiples of 5: " +countBuzz + "\n Multiples of both 3 and 5 (15) : "+countFizzBuzz);
 }
 
 
@@ -122,4 +143,4 @@ const buzz = (n) =>{
 
 
 // call the main function
-main(100);
+main(10);
